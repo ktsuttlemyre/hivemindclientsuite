5d7e520236269dfe8d80193d05c32a91ffe0b1c8
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-03-08 20:50:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #Install
+sudo nano /etc/fail2ban/jail.local
+sudo nano /etc/fail2ban/filter.d/http-get-dos.conf
+sudo nano /etc/fail2ban/filter.d/http-post-dos.conf
+#Use these to check if everything is all right:
+
+sudo systemctl restart fail2ban
+
+
+
 
 cat << EOF > /$HOME/.config/rclone.conf
 [GoogleDrive]
