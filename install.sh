690f35fdf7e8fb8537c536494b2c2942618f806e
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-03-10 15:57:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Installing Rogue OS. Some of the commands will need sudo access. Please grant sudo use."
+#do a sudo command to get the password out of the way
+sudo echo "Thank you for granting sudo privileges" || exit 1
+
+sudo apt install rclone fail2ban -y
 #Install
 sudo nano /etc/fail2ban/jail.local
 sudo nano /etc/fail2ban/filter.d/http-get-dos.conf
@@ -7,9 +12,6 @@ sudo nano /etc/fail2ban/filter.d/http-post-dos.conf
 #Use these to check if everything is all right:
 
 sudo systemctl restart fail2ban
-
-
-
 
 cat << EOF > /$HOME/.config/rclone.conf
 [GoogleDrive]
@@ -19,3 +21,6 @@ client_secret = $client_secret
 scope = drive.file
 token = 
 EOF
+
+
+rclone config && echo "Thanks for installing" || echo "There was an error while inistalling"
