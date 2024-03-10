62c3b7bebccda6758d2c8aaefc7afc83239dc80a
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-03-10 16:11:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ echo "Installing Rogue OS. Some of the commands will need sudo access. Please gr
 #do a sudo command to get the password out of the way
 sudo echo "Thank you for granting sudo privileges" || exit 1
 
+source pi.config
+
 sudo apt install rclone fail2ban -y
 #Install
 sudo nano /etc/fail2ban/jail.local
@@ -16,10 +18,9 @@ sudo systemctl restart fail2ban
 cat << EOF > /$HOME/.config/rclone.conf
 [GoogleDrive]
 type = drive
-client_id = $client_id
-client_secret = $client_secret
+client_id = $rclone_client_id
+client_secret = $rclone_client_secret
 scope = drive.file
-token = 
 EOF
 
 
