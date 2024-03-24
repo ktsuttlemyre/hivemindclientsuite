19982ee7431e8b101a2dcac7d177f57ca300c123
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-03-23 22:10:28 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -22,62 +22,64 @@ trunk () {
   echo "$(tail -c $size $file)" > $file
 }
 
-direction="$1"
-case $direction in
-  init )
-    rclone bisync $HOME GoogleDrive:$rclone_root/ --exclude-from "~/$project/excludes.txt" --resync --resync-mode newer --create-empty-src-dirs --slow-hash-sync-only --resilient -Mv --drive-skip-gdocs --fix-case | to_log rclone.log
-  ;;
-  sync )
-    ./sync.sh status
-    rclone bisync $HOME GoogleDrive:${rclone_root}/ --exclude-from "~/$project/excludes.txt"--resilient --recover --max-lock 2m --conflict-resolve newer | to_log rclone.log
-  ;;
-  upload )
-    rclone sync ~/ GoogleDrive:$rclone_root --exclude node_modules | to_log rclone.log
-  ;;
-  download )
-  if rclone sync GoogleDrive:$rclone_root ~/ --exclude node_modules; then #if 0 successful and files changed, 9 is successfull no file change
-    ./sync.sh restart
-  fi
-  ;;
-  restart )
-    sudo systemctl restart hivemind-client
-    sudo systemctl restart hivemind-nfc-reader
-    sudo cp ~/wpa_supplicant /boot/
-  ;;
-  status )
-    file=./command.txt
-    if [ -f $file ] && [ -s $file ]; then
-      echo "> $(cat $file)" | to_log ./command.output.txt
-      bash $file | to_log ./command.output.txt
-      echo "" > $file
+sync () {
+  direction="$1"
+  case $direction in
+    init )
+      rclone bisync $HOME GoogleDrive:$rclone_root/ --exclude-from "~/$project/excludes.txt" --resync --resync-mode newer --create-empty-src-dirs --slow-hash-sync-only --resilient -Mv --drive-skip-gdocs --fix-case | to_log rclone.log
+    ;;
+    sync )
+      sync status
+      rclone bisync $HOME GoogleDrive:${rclone_root}/ --exclude-from "~/$project/excludes.txt"--resilient --recover --max-lock 2m --conflict-resolve newer | to_log rclone.log
+    ;;
+    upload )
+      rclone sync ~/ GoogleDrive:$rclone_root --exclude node_modules | to_log rclone.log
+    ;;
+    download )
+    if rclone sync GoogleDrive:$rclone_root ~/ --exclude node_modules; then #if 0 successful and files changed, 9 is successfull no file change
+      ./sync.sh restart
     fi
-
-    curl -s ipinfo.io | to_log ipinfo.io
-    traceroute google.com | to_log traceroute.txt
-    
-    #get external ip
-    declare -a cmd=('curl -s -4 icanhazip.com' \
-      'curl -s ifconfig.me' \
-      'curl -s api.ipify.org' \
-      'curl -s bot.whatismyipaddress.com' \
-      'curl -s ipinfo.io/ip' \
-      'curl -s ipecho.net/plain')
-    ip=''
-    for i in "${cmd[@]}"; do
-      ip=$($i)
-      if ! [ -z "$ip" ]; then
-        break
+    ;;
+    restart )
+      sudo systemctl restart hivemind-client
+      sudo systemctl restart hivemind-nfc-reader
+      sudo cp ~/wpa_supplicant /boot/
+    ;;
+    status )
+      file=./command.txt
+      if [ -f $file ] && [ -s $file ]; then
+        echo "> $(cat $file)" | to_log ./command.output.txt
+        bash $file | to_log ./command.output.txt
+        echo "" > $file
       fi
-    done
-    echo "$ip" | to_log ip_public.txt
-
-    ifconfig | to_log ifconfig.txt
-    sudo iwlist wlan0 scan | to_log iwlist.txt
-    ip route | grep -Eo '([0-9]*\.){3}[0-9]*' | sed "2q;d" | to_log ip_private.txt
-    echo "$DATE" | to_log sync_ran.txt
-  ;;
-  * )
-    echo "unknkown direction $direction"
-  ;;
-esac
-
+  
+      curl -s ipinfo.io | to_log ipinfo.io
+      traceroute google.com | to_log traceroute.txt
+      
+      #get external ip
+      declare -a cmd=('curl -s -4 icanhazip.com' \
+        'curl -s ifconfig.me' \
+        'curl -s api.ipify.org' \
+        'curl -s bot.whatismyipaddress.com' \
+        'curl -s ipinfo.io/ip' \
+        'curl -s ipecho.net/plain')
+      ip=''
+      for i in "${cmd[@]}"; do
+        ip=$($i)
+        if ! [ -z "$ip" ]; then
+          break
+        fi
+      done
+      echo "$ip" | to_log ip_public.txt
+  
+      ifconfig | to_log ifconfig.txt
+      sudo iwlist wlan0 scan | to_log iwlist.txt
+      ip route | grep -Eo '([0-9]*\.){3}[0-9]*' | sed "2q;d" | to_log ip_private.txt
+      echo "$DATE" | to_log sync_ran.txt
+    ;;
+    * )
+      echo "unknkown direction $direction"
+    ;;
+  esac
+}
+sync "$1"
